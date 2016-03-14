747d43822e875834e741560c6a123f0f236a12bd
eluck <mail@eluck.me>
2016-03-14 12:30:05 +0300

    Add a snippet for triggering keyboard events from vanilla js

---
+++ snippets/trigger-keyboard-event.js
@@ -0,0 +1,16 @@
+function __triggerKeyboardEvent(el, keyCode)
+{
+    var eventObj = document.createEventObject ?
+        document.createEventObject() : document.createEvent("Events");
+  
+    if(eventObj.initEvent){
+      eventObj.initEvent("keydown", true, true);
+    }
+  
+    eventObj.keyCode = keyCode;
+    eventObj.which = keyCode;
+    
+    el.dispatchEvent ? el.dispatchEvent(eventObj) : el.fireEvent("onkeydown", eventObj); 
+  
+}
+__triggerKeyboardEvent(document.body, 39);
